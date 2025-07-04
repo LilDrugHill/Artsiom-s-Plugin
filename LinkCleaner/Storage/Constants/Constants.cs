279d8145d123633e11adb42b806be75d883be381
LilDrugHill <user@WIN-HAGEL6GDGVN>
2025-07-04 14:45:05 +0300

    Generic storage for xml fields added

---
+++ LinkCleaner/Storage/Constants/Constants.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+
+namespace LinkCleaner.Storage.Constants
+{
+    public static class ConfigConstants
+    {
+        public static readonly string PathToConfigurationDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "LinkCleaner");
+        public static readonly string PathToConfigurationFile = Path.Combine(PathToConfigurationDirectory, "MonitoringConfig.xml");
+
+        public const string DocNameField = "Name"; // Name of the document
+        public const string DocGuidField = "Guid";
+        public const string DocStatusField = "Status"; // Status of the document (e.g., Active, Inactive)
+        public const string LinkNameField = "Name"; // Name of the link
+        public const string LinkGuidField = "Guid"; // Unique identifier for the link
+        public const string LinkIsMonitoringField = "IsMonitoring"; // Indicates if the link is being monitored
+
+        public const string RootName = "Data"; // Root element for the XML configuration file
+        public const string ProjectNodeName = "Project";
+        public const string LinkNodeName = "Link";
+
+
+    }
+}
